5116decda7ddc2ef221223812b1872f230b6a135
Emanuel Epifani <emanuel.epifani.m@gmail.com>
2022-04-09 12:51:09 +0200

    add lezione Sabato non finito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { HeroDetailComponent } from './components/hero-detail/hero-detail.compon
 import { MessagesComponent } from './components/messages/messages.component';
 import { AppRoutingModule } from './services/app-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,14 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HEROESMOCKDATA } from '../mock-data/mock-heroes';
@@ -8,16 +9,20 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
 
   constructor(
-    private messageService: MessageService //inietto la classe "MessageService" e la immagazzino dentro la variabile messageService
+    private messageService: MessageService, //inietto la classe "MessageService" e la immagazzino dentro la variabile messageService
+    private http: HttpClient
   ) { }
 
   //funzione che poi potrò richiamare dal controller 
   getHeroes(): Observable<Hero[]> { //
     //return this.http.get('url-con-i-dati)  -> qst se avro una url per un API
-    const HEROES = of(HEROESMOCKDATA);
-    this.messageService.add("Heroservice: fetched heroes");
+    const HEROES =  this.http.get<Hero[]>(this.heroesUrl);
+    
+    this.log("fetched heroes");
+
     return HEROES;
   }
 
@@ -25,8 +30,13 @@ export class HeroService {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
     const hero = HEROESMOCKDATA.find( h => h.id === selectedId)!;
-    this.messageService.add(`HeroService: fetched hero id=${selectedId}`);
+    this.log(`fetched hero id=${selectedId}`);
     return of(hero);
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
 }
