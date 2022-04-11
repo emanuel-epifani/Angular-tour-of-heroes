e913211cde72c5196f36f24891bf2a45dd93f297
Emanuel Epifani <emanuel.epifani.m@gmail.com>
2022-04-11 12:32:41 +0200

    update Heroes 6. get data from a server

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -4,6 +4,7 @@ import { Hero } from 'src/app/models/hero';
 import { Location } from '@angular/common';
 import { HeroService } from 'src/app/services/hero.service';
 
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -28,6 +29,16 @@ export class HeroDetailComponent implements OnInit {
     //console.log('id=', id);
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
+
+
+  
 
   goBack(): void {
     this.location.back(); 
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,15 +1,19 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HEROESMOCKDATA } from '../mock-data/mock-heroes';
 import { Hero } from '../models/hero';
 import { MessageService } from './message.service';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   private heroesUrl = 'api/heroes';  // URL to web api
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(
     private messageService: MessageService, //inietto la classe "MessageService" e la immagazzino dentro la variabile messageService
@@ -18,25 +22,60 @@ export class HeroService {
 
   //funzione che poi potrò richiamare dal controller 
   getHeroes(): Observable<Hero[]> { //
-    //return this.http.get('url-con-i-dati)  -> qst se avro una url per un API
-    const HEROES =  this.http.get<Hero[]>(this.heroesUrl);
-    
-    this.log("fetched heroes");
+    return  this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes ')),
+        catchError( error => { 
+          console.error(error);
 
-    return HEROES;
+          this.log(`getHeroes failed: ${error.message}`);
+
+          return of([]);
+        })
+      );
+
   }
+
 
   getHero(selectedId: number): Observable<Hero> {
-    // For now, assume that a hero with the specified `id` always exists.
-    // Error handling will be added in the next step of the tutorial.
-    const hero = HEROESMOCKDATA.find( h => h.id === selectedId)!;
-    this.log(`fetched hero id=${selectedId}`);
-    return of(hero);
+    return this.http.get<Hero>(this.heroesUrl +'/'+ selectedId)
+    .pipe(
+      tap(_=> this.log(`fetched hero id = ${selectedId}`)),
+      catchError(error => {
+          console.error(error);
+
+          this.log(`getHero id=${selectedId} failed:${error.status}:  ${error.body.error}`);
+
+          return of();
+      }),
+    )
   }
+
+
+/** PUT: update the hero on the server */
+updateHero(hero: Hero): Observable<any> {
+  return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap(_ => this.log(`updated hero id=${hero.id}`)),
+    catchError(error => {
+        console.error(error);
+
+        this.log(`updateHero failed:${error.status}:  ${error.body.error}`);
+
+        return of();
+    })
+    
+
+  );
+}
+ 
+
 
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
 
+
+
+  
 }
